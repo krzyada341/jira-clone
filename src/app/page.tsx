a546155ba43b3e923e3211c2e09e59d8af7c6d48
krzyada341 <krzyada341@student.polsl.pl>
2026-02-01 23:25:16 +0100

    Setting up appwrite,  database and session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { useCurrent } from '@/features/auth/api/use-current'
+import { useLogout } from '@/features/auth/api/use-logout'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Home() {
+	const router = useRouter()
+	const { data, isLoading } = useCurrent()
+	const { mutate } = useLogout()
+
+	useEffect(() => {
+		if (!data && !isLoading) {
+			router.push('/sign-in')
+		}
+	}, [data])
+
 	return (
 		<div className="flex gap-4">
-			<Input />
-			<Button>Primaty</Button>
-			<Button variant={'secondary'}>Secondary</Button>
-			<Button variant={'destructive'}>destructive</Button>
-			<Button variant={'ghost'}>ghost</Button>
-			<Button variant={'outline'}>outline</Button>
-			<Button variant={'muted'}>muted</Button>
-			<Button variant={'tertiary'}>tertiary</Button>
+			Only visible to users <Button onClick={() => mutate()}>Logout</Button>
 		</div>
 	)
 }
